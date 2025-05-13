34f5ca5d6dd0fc0ec7c8621e497855928821b117
R'laf | 阿乐夫 <rlaf9901@gmail.com>
2025-05-13 20:41:19 +0800

    Replace Taut/ with git-remote-taut/

---
+++ git-remote-taut/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleAppFramework;
+
+ConsoleApp.Version = "alpha-0.0.1";
+
+var app = ConsoleApp.Create();
+app.Add<MyCommands>();
+app.Run(args);
+
+public class MyCommands
+{
+    /// <summary>Root command test.</summary>
+    [Command("")]
+    public void Root() => Console.WriteLine("git-remote-taut");
+
+    /// <summary>Display message.</summary>
+    /// <param name="msg">Message to show.</param>
+    public void Echo(string msg) => Console.WriteLine(msg);
+
+    /// <summary>Sum parameters.</summary>
+    /// <param name="x">left value.</param>
+    /// <param name="y">right value.</param>
+    public void Sum(int x, int y) => Console.WriteLine(x + y);
+}
